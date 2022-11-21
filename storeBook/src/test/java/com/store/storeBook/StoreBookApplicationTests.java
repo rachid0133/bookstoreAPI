22151a2f45ef02af4d9a13117a6b360a2a4f534c
rachid chakor <chakor.rachid.1@gmail.com>
2022-11-21 16:03:40 -0500

    storeBook

---
+++ storeBook/src/test/java/com/store/storeBook/StoreBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.storeBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
